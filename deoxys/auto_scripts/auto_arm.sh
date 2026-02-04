522cf6b72e6b4ad1c0c98946c9f34896bf176b57
Jeremiah Coholich <jcoholich@gatech.edu>
2026-02-04 18:18:35 -0500

    debug robot script terminations

--- deoxys/auto_scripts/auto_arm.sh
+++ deoxys/auto_scripts/auto_arm.sh
@@ -2,6 +2,37 @@
 . $(dirname "$0")/color_variables.sh
 . $(dirname "$0")/fix_ld_issue.sh
 
+DEBUG_MODE="off"
+ENABLE_COREDUMP=1
+
+while [[ "$1" == --* ]]; do
+    case "$1" in
+        --gdb)
+            DEBUG_MODE="gdb"
+            shift
+            ;;
+        --gdb-bt)
+            DEBUG_MODE="gdb-bt"
+            shift
+            ;;
+        --no-coredump)
+            ENABLE_COREDUMP=0
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ "$ENABLE_COREDUMP" -eq 1 ]; then
+    ulimit -c unlimited 2>/dev/null
+fi
+
 printf "${BIRed} Make sure you are in the Performance Mode!!! ${Color_Off} \n"
 
 RTOS_MODE=$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor)
@@ -15,8 +46,27 @@ do
     fi
 done
 
+run_franka_interface() {
+    if [ "$DEBUG_MODE" = "gdb" ]; then
+        gdb --args bin/franka-interface "$@"
+    elif [ "$DEBUG_MODE" = "gdb-bt" ]; then
+        gdb -q -ex run -ex "bt" -ex "thread apply all bt" -ex quit --args bin/franka-interface "$@"
+    else
+        bin/franka-interface "$@"
+    fi
+}
+
 while true
 do
-    bin/franka-interface $@
+    run_franka_interface "$@"
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "franka-interface exited with code $status; stopping auto-restart."
+        if [ "$ENABLE_COREDUMP" -eq 1 ]; then
+            echo "Core dumps enabled. If a core file exists, inspect with: gdb bin/franka-interface core"
+            echo "If no core file appears, try: coredumpctl info bin/franka-interface"
+        fi
+        exit $status
+    fi
     sleep 1
 done
